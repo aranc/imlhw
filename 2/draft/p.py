f237f66acbb28b1ae3170292d3e896a402e0a0ea
Aran Carmon <aran.carmon@gmail.com>
2016-12-11 09:40:09 +0200

    more

--- 2/draft/p.py
+++ 2/draft/p.py
@@ -1,12 +1,34 @@
 import sys
+import time
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from hw2 import *
 
 def build_preceptron(samples, labels):
-    pass
-    return w
+    w = np.zeros(len(samples[0]))
+    for i in range(len(samples)):
+        predicted = classify(w, samples[i])
+        if predicted != labels[i]:
+            w += labels[i] * samples[i]
 
 def classify(w, x):
-    return 0
+    return 1 if np.dot(w, x) >= 0 else -1
+
+def go():
+    print "building preceptron"
+    start = time.time()
+    w = build_preceptron(train_data, train_labels)
+    print "done. elapsed:", time.time() - start
+
+    print "testing"
+    start = time.time()
+    errors = 0
+    for i in range(len(test_data)):
+        predicted = classify(w, test_data[i])
+        if predicted != test_labels[i]:
+            errors += 1
+    print "done. success rate:", 1 - float(errors)/float(len(test_data)), "elapsed:", time.time() - start
+
+
