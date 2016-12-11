99c1a98cd71dcae2caf44b8012b52e7e3f6dfdda
Aran Carmon <aran.carmon@gmail.com>
2016-12-11 09:23:05 +0200

    more

---
+++ 2/draft/p.py
@@ -0,0 +1,12 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from hw2 import *
+
+def build_preceptron(samples, labels):
+    pass
+    return w
+
+def classify(w, x):
+    return 0
