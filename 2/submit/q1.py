cfff807ddb0fc1ab3952ee734cff5e18d4d03f05
Aran Carmon <aran.carmon@gmail.com>
2016-12-23 16:46:25 +0200

    more

--- 2/submit/q1.py
+++ 2/submit/q1.py
@@ -6,18 +6,52 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from hw2 import *
 
+def build_perceptron(samples, labels):
+    w = np.zeros(len(samples[0]))
+    for i in range(len(samples)):
+        predicted = classify(w, samples[i])
+        if predicted != labels[i]:
+            w += labels[i] * samples[i] / np.linalg.norm(samples[i])
+    return w
 
+def classify(w, x):
+    x = x / np.linalg.norm(x)
+    return 1 if np.dot(w, x) >= 0 else -1
+
+#Test / Validate weights w
+def measure(w, data, labels):
+    errors = 0
+    for i in range(len(data)):
+        predicted = classify(w, data[i])
+        if predicted != labels[i]:
+            errors += 1
+    return 1 - float(errors)/float(len(data))
+
+
+#Run a measurement for question 1a, for a specific n value
+def q1a_helper(n):
+    T=100
+    data = train_data[:n]
+    labels = train_labels[:n]
+    res = np.zeros(T)
+    for i in range(T):
+        idx = np.random.permutation(n)
+        w = build_perceptron(data[idx], labels[idx])
+        res[i] = measure(w, test_data, test_labels)
+    res = np.sort(res)
+    #return mean, 5 precentile, and 95 precentile
+    return np.mean(res), res[5], res[95]
+
+def q1a():
+    print "List of n, mean accuracy, 5 & 95 precentiles"
+    for n in [5, 10, 50, 100, 500, 1000, 5000]:
+        print n, q1a_helper(n)
 
 if True:
     #Get subquestion from first argument
     if sys.argv[1] == 'a':
-        #get _from, _to, _step, and plot output filename from remaining arguments
-        _from = float(sys.argv[2])
-        _to = float(sys.argv[3])
-        _step = float(sys.argv[4])
-        output = sys.argv[5]
-        q3a(_from, _to, _step, output)
-    if sys.argv[1] == 'b':
+        q1a()
+    elif sys.argv[1] == 'b':
         #get _from, _to, _step, and plot output filename from remaining arguments
         _from = float(sys.argv[2])
         _to = float(sys.argv[3])
