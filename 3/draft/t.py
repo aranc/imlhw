ac36786440882b0bce004f070efda4e6afeafd99
Aran Carmon <aran.carmon@gmail.com>
2017-01-01 19:00:57 +0200

    more

--- 3/draft/t.py
+++ 3/draft/t.py
@@ -1,11 +1,23 @@
-sgd_train():
+import sys
+import time
+import numpy as np
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from hw3 import *
+
+#Train for question 6
+def svm_sgd_train():
     pass
 
-sgd_classify(k, w, x):
-    
-
-kernel_train():
+#Classify for question 6
+def svm_sgd_classify(k, w, x):
     pass
 
-kernel_classify():
+#Train for question 7
+def svm_kernel_train():
+    pass
+
+#Classify for question 7
+def svm_kernel_classify():
     pass
