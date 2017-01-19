ea9a2399811be29219fb4eebaea5f88fd1479914
Aran Carmon <aran.carmon@gmail.com>
2017-01-19 21:44:32 +0200

    more

--- 4/submit/ada.py
+++ 4/submit/ada.py
@@ -7,6 +7,55 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from hw4 import *
 
+#Build weak learner from parameters
+def build_weak_learner(pixel, theta, direction):
+    if direction:
+        return lambda x: -1 if x[pixel] <= theta else 1
+    else:
+        return lambda x: 1 if x[pixel] <= theta else -1
+
+#Measure error of classifier
+def measure(classifier, data, labels):
+    errors = 0
+    for i in range(len(data)):
+        if labels[i] != classifier(data[i]):
+            errors += 1
+    return float(errors) / float(len(data))
+
+#Return the best weak learner for the given distribution
+def find_best_weak_learner(data, labels, distribution):
+    #Consts
+    pixels = 28
+    
+    #Parameters to argmin
+    best_pixel = 0
+    best_theta = 0
+    best_direction = False
+    best_error = float("inf")
+
+    #Find argmin error
+    start = time.time()
+    for pixel = range(pixels):
+        possible_thetas = np.unique(data[:,pixel])
+        for theta in possible_thetas:
+            for direction in (True, False):
+                learner = build_weak_learner(pixel, theta, direction)
+                error = measure(learner, data, labels)
+                if error < best_error:
+                    best_error = error
+                    best_direction = direction
+                    best_theta = theta
+                    best_pixel = pixel
+
+    print "find_best_weak_learner: elapsed:", time.time() - start, "error:", best_error, "pixel:", best_pixel, "theta:", best_theta, "direction:", best_direction
+
+    return build_weak_learner(best_pixel, best_theta, best_direction)
+
+
+#Perform an additional iteration of adaboost
+def adaboost_iteration(data, labels, distribution):
+    pass
+
 if False:
     if len(sys.argv) > 1 and sys.argv[1] == 'bla':
         _from = float(sys.argv[3])
