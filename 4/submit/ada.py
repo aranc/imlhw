9d6921b2253a6b797cd609ec01a191513db0a590
Aran Carmon <aran.carmon@gmail.com>
2017-01-20 00:31:38 +0200

    more

--- 4/submit/ada.py
+++ 4/submit/ada.py
@@ -91,6 +91,23 @@ def build_linear_classifier(a, h):
         return -1 if _sum < 0 else 1
     return classifier
 
+
+#Calculate average exponential loss
+def calc_average_exponential_loss(a, h, data, labels):
+    m = len(data)
+    T = len(h)
+
+    ael_error = 0
+    for i in range(m):
+        exponent = 0
+        for j in range(T):
+            exponent += a[j] * h[j](x[i])
+        exponent *= -y[i]
+        ael_error += ( e**exponent ) / m
+
+    return ael_error
+
+
 #Run T iterations of adaboost, prepare plots for subquestions a and b
 def answer_subquestions(T, plot_a, plot_b):
     #Init uniform distribution
@@ -103,6 +120,8 @@ def answer_subquestions(T, plot_a, plot_b):
     #List for the plots
     training_errors = []
     test_errors = []
+    ael_training_errors = []
+    ael_test_errors = []
 
     #Run T iterations
     for t in range(T):
@@ -112,7 +131,27 @@ def answer_subquestions(T, plot_a, plot_b):
         classifier = build_linear_classifier(a, h)
         training_errors.append(measure(classifier, train_data, train_labels))
         test_errors.append(measure(classifier, test_data, test_labels))
-        print "training error:", training_errors[-1], "testing_error:", test_errors[-1]
+        ael_training_error.append(calc_average_exponential_loss())
+        ael_test_error.append(calc_average_exponential_loss())
+        print "t:", t, "training error:", training_errors[-1], "testing_error:", test_errors[-1]
+
+    #Prepare plots
+    plt.title("Training and Test errors")
+    plt.gca().set_xlabel("T iterations")
+    plt.gca().set_ylabel("Error")
+    plt.plot(range(1,T+1), test_errors, 'ko', label="Test Error")
+    plt.plot(range(1,T+1), training_errors, 'k*', label="Training Error")
+    plt.legend()
+    plt.savefig(plot_a)
+
+    plt.clf()
+    plt.title("Training and Test average exponential loss")
+    plt.gca().set_xlabel("T iterations")
+    plt.gca().set_ylabel("AEL")
+    plt.plot(range(1,T+1), ael_test_errors, 'ko', label="Test AEL")
+    plt.plot(range(1,T+1), ael_training_errors, 'k*', label="Training AEL")
+    plt.legend()
+    plt.savefig(plot_a)
 
 if True:
     T = int(sys.argv[1])
