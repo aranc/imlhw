035360016be9790c4c64f3c253a5c953a0756b48
Aran Carmon <aran.carmon@gmail.com>
2017-01-28 19:38:45 +0200

    more

---
+++ 5/submit/q.py
@@ -0,0 +1,38 @@
+import sys
+import time
+import operator
+import random
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from hw5 import *
+
+
+if False:
+    if len(sys.argv) > 1 and sys.argv[1] == 'a':
+        #We output 7 plots for subquestions a,b, and c.
+        #Plot the mea, plot 5 eigenvectors, and plot the eigenvalues
+        filenames = [None] * 7
+        filenames[0] = sys.argv[2]
+        filenames[1] = sys.argv[3]
+        filenames[2] = sys.argv[4]
+        filenames[3] = sys.argv[5]
+        filenames[4] = sys.argv[6]
+        filenames[5] = sys.argv[7]
+        filenames[6] = sys.argv[8]
+        subquestion_a(filenames)
+    elif len(sys.argv) > 1 and sys.argv[1] == 'b':
+        #We output 7 plots for subquestions a,b, and c.
+        #Plot the mea, plot 5 eigenvectors, and plot the eigenvalues
+        filenames = [None] * 7
+        filenames[0] = sys.argv[2]
+        filenames[1] = sys.argv[3]
+        filenames[2] = sys.argv[4]
+        filenames[3] = sys.argv[5]
+        filenames[4] = sys.argv[6]
+        filenames[5] = sys.argv[7]
+        filenames[6] = sys.argv[8]
+        subquestion_b(filenames)
+    else:
+        print "Error: please choose a valid command"
