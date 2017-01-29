9b64b2c2ce1c7399c5665a407f10e005975ddb10
Aran Carmon <aran.carmon@gmail.com>
2017-01-30 00:02:29 +0200

    more

--- 5/submit/q.py
+++ 5/submit/q.py
@@ -9,6 +9,41 @@ import matplotlib.pyplot as plt
 from hw5 import *
 from scipy.misc import logsumexp
 
+#Perform an EM step
+#ss stands for sigma squared
+#c stands for the prior
+#Modifies mu, ss, c vectors
+def do_em_step(x, mu, ss, c):
+    #Get n,k consts from input
+    n = len(x)
+    k = len(mu)
+
+    #Calc p(z_i = m | x_i, theta)
+    p = np.zeros((n, k))
+    for i in range(n):
+        for m in range(k):
+            p[i, m] = (2*pi)**(-k/2.0)
+            p[i, m] *= ss[m] ** (-1.0/2.0)
+            p[i, m] *= e ** (-(np.norm(x[i]-mu[m]))/(2*ss[m]))
+        p[i,:] /= p[i].sum()
+
+    #Calc new c
+    for m in range(k):
+        c[m] = (1.0/n) * p[:,m].sum()
+
+    #Calc new mu
+    for m in range(k):
+        mu[m] = 0
+        for i in range(n):
+            mu[m] += p[i, m] * x[i]
+        mu[m] /= p[:,m].sum()
+
+    #Calc new ss
+    for m in range(k):
+        ss[m] = 0
+        for i in range(n):
+            ss[m] += p[i, m] * np.norm(x[i] - mu[m]
+        ss[m] /= p[:m].sum()
 
 if False:
     if len(sys.argv) > 1 and sys.argv[1] == 'a':
